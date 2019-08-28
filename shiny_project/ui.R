381c9771da5616ba3e82f8b6581e89668f599634
Nikolaos Perdikis <nikolas.perdikis@gmail.com>
2019-08-28 18:51:15 +0200

    updates

--- shiny_project/ui.R
+++ shiny_project/ui.R
@@ -10,13 +10,12 @@ shinyUI(fluidPage(
         ),
         mainPanel(
             tabsetPanel(type="tabs",
-            tabPanel("Help",br(),"Welcome to the application.",br(), "Use the slider to change population value",br(),
-                     "The application will display the estimated Unemployment for that value",br(),
-                     "You may choose to check/uncheck the Linear Model display",br(), 
+            tabPanel("Help",br(),h4("Welcome to the application!"),br(), "Use the slider to change population value",br(),
+                       "You may choose to check/uncheck the Linear Model display",br(), 
                      "Click on Submit to finalize your choices and let the app do server side calculations",br(),
-                     "Next, click on the Output tab to display the results.",br(),
-                     "You may return to the slider to do further calculations as desired.") ,           
-            tabPanel("Output" ,
+                     "You may return to the slider to do further calculations as desired." ,br(), 
+                    h4("Last, click on the Output tab to display the results.")),
+           tabPanel("Output" ,
             plotOutput("plot1"),
             h3("Predicted Unemployment from Model:"),
             textOutput("pred1"))
