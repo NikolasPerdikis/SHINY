a934b67c78c5048b0d7f5bb62a85259741af3b7b
Nikolaos Perdikis <nikolas.perdikis@gmail.com>
2019-08-28 15:35:30 +0200

    Update ui.R
    added help tab

--- shiny_project/ui.R
+++ shiny_project/ui.R
@@ -9,9 +9,15 @@ shinyUI(fluidPage(
             submitButton("Submit")
         ),
         mainPanel(
+            tabsetPanel(type="tabs",
+            tabPanel("Help",br(),"Welcome to the application. Use slider above to change population value
+                     and check/uncheck the Linear Model display, then click on Submit. Next, 
+                     click on the Output tab to display the results.") ,           
+            tabPanel("Output" ,
             plotOutput("plot1"),
             h3("Predicted Unemployment from Model:"),
-            textOutput("pred1")
+            textOutput("pred1"))
+        )
         )
     )
 ))
